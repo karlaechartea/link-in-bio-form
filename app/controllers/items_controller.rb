97bc4121f42cd66adecd1de16641663fdb379327
karlaechartea <k.echartea@chicagobooth.edu>
2024-11-07 00:10:59 +0000

    save1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,17 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+
+  def display_form
+    @list_of_items = Item.all
+
+    render({ :template => "item_templates/backdoor_template" })
+  end
+
+  def new_form
+    @list_of_items = Item.all
+
+    render({ :template => "item_templates/backdoor_template" })
+  end
 end
