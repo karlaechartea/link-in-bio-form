acfe74ecbc5bb992961f1429f5960ee28aa81b6a
karlaechartea <k.echartea@chicagobooth.edu>
2024-11-13 17:40:19 +0000

    save

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,8 +13,13 @@ class ItemsController < ApplicationController
   end
 
   def new_form
-    @list_of_items = Item.all
+    item = Item.new
+    item.link_url =params.fetch("link_url")
+    item.link_description =params.fetch("link_description")
+    item.thumbnail_url =params.fetch("thumbnail_url")
 
-    render({ :template => "item_templates/backdoor_template" })
+    item.save
+
+    redirect_to("/")
   end
 end
